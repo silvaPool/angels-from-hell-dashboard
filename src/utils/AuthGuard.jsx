5233bf3c2bec00370fc4855ca810309a034fd453
silvaPool <silva.friends22@gmail.com>
2024-07-29 09:38:55 -0300

    Componente do authGuard criado

---
+++ src/utils/AuthGuard.jsx
@@ -0,0 +1,18 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function AuthGuard({children}) {
+    const {signed} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!signed) {
+            navigate("auth/login");
+        }
+    }, [signed, navigate]);
+
+    return children;
+}
+
+export default AuthGuard;
