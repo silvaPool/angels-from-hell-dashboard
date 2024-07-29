fb7f22a0d4a47d36485dfc3ba39b2b31594818e0
silvaPool <silva.friends22@gmail.com>
2024-07-29 09:15:51 -0300

    função de login de email e senha

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { auth } from "../services/FirebaseConfig";
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 export const AuthContext = createContext({});
 
@@ -10,6 +10,21 @@ const authRef = auth;
 export const Auth = ({children}) => {
 
     const [user, setUser] = useState(null);
+
+    async function login(email, senha) {
+        try {
+            const response = await signInWithEmailAndPassword(authRef, email, senha);
+
+            setUser(response.user);
+
+            console.log(response);
+
+            return true;
+
+        } catch (error) {
+            return error.message;
+        }
+    }
 
 
     async function signUp(email, senha) {
