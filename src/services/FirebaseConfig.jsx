3d8b3d152063d1b803fb97271da823a09d57f0f7
silvaPool <silva.friends22@gmail.com>
2024-07-29 08:58:59 -0300

    arquivo firebaseConfig

---
+++ src/services/FirebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpMfVeaTYP8_8orGOVpkqaeRR4lDOhcxY",
+  authDomain: "angels-from-hell-dash.firebaseapp.com",
+  projectId: "angels-from-hell-dash",
+  storageBucket: "angels-from-hell-dash.appspot.com",
+  messagingSenderId: "862734770998",
+  appId: "1:862734770998:web:606ee4279fee37b7c08997"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
