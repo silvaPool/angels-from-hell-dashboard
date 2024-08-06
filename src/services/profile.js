ec576371c543f056ff0bd341f5c3bb60829dab36
silvaPool <silva.friends22@gmail.com>
2024-08-06 10:30:53 -0300

    função setPhoto

--- src/services/profile.js
+++ src/services/profile.js
@@ -1,4 +1,5 @@
 import {  setDoc, doc, updateDoc, getDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export async function setProfileData(profile, uid) {
     
@@ -24,4 +25,20 @@ export async function getProfile(uid) {
     const profileUserRef = doc(db, "profile", uid);
     const docSnap = await getDoc(profileUserRef);
     return docSnap.data();
+}
+
+export async function setPhoto(file, uid) {
+    try {
+        const storageRef = ref(storage, uid);
+
+        const snapshot = await uploadBytes(storageRef, file);
+
+        const urlDownloadData = await getDownloadURL(storageRef, snapshot);
+        console.log(urlDownloadData);
+        await updateProfile({urlImage:urlDownloadData}, uid);
+
+        return urlDownloadData;
+    } catch (error) {
+        return error;
+    }
 }
