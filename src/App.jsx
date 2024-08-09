61f84ecc0be4338e86bc1df10e84b7d4ada6b73f
silvaPool <silva.friends22@gmail.com>
2024-08-09 11:16:49 -0300

    Andamento dos estilos no componente paginaArtista

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
 
       {/* <HomeUsuario /> */}
 
-      <Arte />
+      {/* <Arte /> */}
 
-      {/* <PaginaArtista /> */}
+      <PaginaArtista />
       
     </>
   )
--- src/components/PaginaArtista.jsx
+++ src/components/PaginaArtista.jsx
@@ -1,9 +1,262 @@
+import { Box, Stack, Typography } from "@mui/material";
+import Morcego from "../assets/images/morcego.png";
+import Witch from "../assets/images/witch.png";
+import Edgar from "../assets/images/edgar.jpeg";
+
 function PaginaArtista() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Box sx={{ border: "1px solid black", padding: "15px" }}>
+      <img src={Morcego} alt="" />
+      <img src={Witch} alt="" />
+      <Stack sx={{ border: "1px solid red", width: "70vw", margin: "0 auto" }}>
+        <Typography sx={{ textAlign: "center", fontFamily: "Cormorant Garamond" }}>Edgar A. Poe</Typography>
+        <Typography class="text">
+          Edgar Allan Poe’s stature as a major figure in world literature is
+          primarily based on his ingenious and profound short stories, poems,
+          and critical theories, which established a highly influential
+          rationale for the short form in both poetry and fiction. Regarded in
+          literary histories and handbooks as the architect of the modern short
+          story, Poe was also the principal forerunner of the “art for art’s
+          sake” movement in 19th-century European literature. Whereas earlier
+          critics predominantly concerned themselves with moral or ideological
+          generalities, Poe focused his criticism on the specifics of style and
+          construction that contributed to a work’s effectiveness or failure. In
+          his own work, he demonstrated a brilliant command of language and
+          technique as well as an inspired and original imagination. Poe’s
+          poetry and short stories greatly influenced the French Symbolists of
+          the late 19th century, who in turn altered the direction of modern
+          literature.
+        </Typography>
+        <Stack
+          spacing={2}
+          sx={{
+            marginTop: "15px",
+            border: "1px solid red",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <img src={Edgar} alt="Imagem do Edgar" width={"400px"} />
+          <Typography sx={{ alignSelf: "center", fontFamily: "UnifrakturMaguntia" }}>
+            Regarded in literary histories and handbooks as the architect of the
+            modern short story, Poe was also the principal forerunner of the
+            “art for art’s sake” movement in 19th-century European literature.
+          </Typography>
+        </Stack>
+        <Typography class="text">
+          Poe’s father and mother were professional actors. At the time of his
+          birth in 1809, they were members of a repertory company in Boston.
+          Before Poe was three years old both of his parents died, and he was
+          raised in the home of John Allan, a prosperous exporter from Richmond,
+          Virginia, who never legally adopted his foster son. As a boy, Poe
+          attended the best schools available, and was admitted to the
+          University of Virginia at Charlottesville in 1825. While there he
+          distinguished himself academically but was forced to leave after less
+          than a year because of bad debts and inadequate financial support from
+          Allan. Poe’s relationship with Allan disintegrated upon his return to
+          Richmond in 1827, and soon after Poe left for Boston, where he
+          enlisted in the army and also published his first poetry collection,
+          Tamerlane, and Other Poems. The volume went unnoticed by readers and
+          reviewers, and a second collection, Al Aaraaf, Tamerlane, and Minor
+          Poems, received only slightly more attention when it appeared in 1829.
+          That same year Poe was honorably discharged from the army, having
+          attained the rank of regimental sergeant major, and was then admitted
+          to the United States Military Academy at West Point. However, because
+          Allan would neither provide his foster son with sufficient funds to
+          maintain himself as a cadet nor give the consent necessary to resign
+          from the Academy, Poe gained a dismissal by ignoring his duties and
+          violating regulations. He subsequently went to New York City, where
+          Poems, his third collection of verse, was published in 1831, and then
+          to Baltimore, where he lived at the home of his aunt, Mrs. Maria
+          Clemm. Over the next few years Poe’s first short stories appeared in
+          the Philadelphia Saturday Courier and his “MS. Found in a Bottle” won
+          a cash prize for best story in the Baltimore Saturday Visitor.
+          Nevertheless, Poe was still not earning enough to live independently,
+          nor did Allan’s death in 1834 provide him with an inheritance. The
+          following year, however, his financial problems were temporarily
+          alleviated when he accepted an editorship at The Southern Literary
+          Messenger in Richmond, bringing with him his aunt and his 12-year-old
+          cousin Virginia, whom he married in 1836. The Southern Literary
+          Messenger was the first of several journals Poe would direct over the
+          next 10 years and through which he rose to prominence as a leading man
+          of letters in America. Poe made himself known not only as a
+          superlative author of poetry and fiction, but also as a literary
+          critic whose level of imagination and insight had hitherto been
+          unapproached in American literature. While Poe’s writings gained
+          attention in the late 1830s and early 1840s, the profits from his work
+          remained meager, and he supported himself by editing Burton’s
+          Gentleman’s Magazine and Graham’s Magazine in Philadelphia and the
+          Broadway Journal in New York City. After his wife’s death from
+          tuberculosis in 1847, Poe became involved in a number of romantic
+          affairs. It was while he prepared for his second marriage that Poe,
+          for reasons unknown, arrived in Baltimore in late September of 1849.
+          On October 3, he was discovered in a state of semi-consciousness; he
+          died four days later without regaining the necessary lucidity to
+          explain what had happened during the last days of his life. Poe’s most
+          conspicuous contribution to world literature derives from the
+          analytical method he practiced both as a creative author and as a
+          critic of the works of his contemporaries. His self-declared intention
+          was to formulate strictly artistic ideals in a milieu that he thought
+          overly concerned with the utilitarian value of literature, a tendency
+          he termed the “heresy of the Didactic.” While Poe’s position includes
+          the chief requisites of pure aestheticism, his emphasis on literary
+          formalism was directly linked to his philosophical ideals: through the
+          calculated use of language one may express, though always imperfectly,
+          a vision of truth and the essential condition of human existence.
+          Poe’s theory of literary creation is noted for two central points:
+          first, a work must create a unity of effect on the reader to be
+          considered successful; second, the production of this single effect
+          should not be left to the hazards of accident or inspiration, but
+          should to the minutest detail of style and subject be the result of
+          rational deliberation on the part of the author. In poetry, this
+          single effect must arouse the reader’s sense of beauty, an ideal that
+          Poe closely associated with sadness, strangeness, and loss; in prose,
+          the effect should be one revelatory of some truth, as in “tales of
+          ratiocination” or works evoking “terror, or passion, or horror.” Aside
+          from a common theoretical basis, there is a psychological intensity
+          that is characteristic of Poe’s writings, especially the tales of
+          horror that comprise his best and best-known works. These
+          stories—which include “The Black Cat,” “The Cask of Amontillado,” and
+          “The Tell-Tale Heart”—are often told by a first-person narrator, and
+          through this voice Poe probes the workings of a character’s psyche.
+          This technique foreshadows the psychological explorations of Fyodor
+          Dostoyevsky and the school of psychological realism. In his Gothic
+          tales, Poe also employed an essentially symbolic, almost allegorical
+          method which gives such works as “The Fall of the House of Usher,”
+          “The Masque of the Red Death,” and “Ligeia” an enigmatic quality that
+          accounts for their enduring interest and links them with the
+          symbolical works of Nathaniel Hawthorne and Herman Melville. The
+          influence of Poe’s tales may be seen in the work of later writers,
+          including Ambrose Bierce and H.P. Lovecraft, who belong to a distinct
+          tradition of horror literature initiated by Poe. In addition to his
+          achievement as creator of the modern horror tale, Poe is also credited
+          with parenting two other popular genres: science fiction and the
+          detective story. In such works as “The Unparalleled Adventure of Hans
+          Pfaall” and “Von Kempelen and His Discovery,” Poe took advantage of
+          the fascination for science and technology that emerged in the early
+          19th century to produce speculative and fantastic narratives which
+          anticipate a type of literature that did not become widely practiced
+          until the 20th century. Similarly, Poe’s three tales of
+          ratiocination—“The Murders in the Rue Morgue,” “The Purloined Letter,”
+          and “The Mystery of Marie Roget”—are recognized as the models which
+          established the major characters and literary conventions of detective
+          fiction, specifically the amateur sleuth who solves a crime that has
+          confounded the authorities and whose feats of deductive reasoning are
+          documented by an admiring associate. Just as Poe influenced many
+          succeeding authors and is regarded as an ancestor of such major
+          literary movements as Symbolism and Surrealism, he was also influenced
+          by earlier literary figures and movements. In his use of the demonic
+          and the grotesque, Poe evidenced the impact of the stories of E.T.A.
+          Hoffman and the Gothic novels of Ann Radcliffe, while the despair and
+          melancholy in much of his writing reflects an affinity with the
+          Romantic movement of the early 19th century. It was Poe’s particular
+          genius that in his work he gave consummate artistic form both to his
+          personal obsessions and those of previous literary generations, at the
+          same time creating new forms which provided a means of expression for
+          future artists. While Poe is most often remembered for his short
+          fiction, his first love as a writer was poetry, which he began writing
+          during his adolescence. His early verse reflects the influence of such
+          English romantics as Lord Byron, John Keats, and Percy Bysshe Shelley,
+          yet foreshadows his later poetry which demonstrates a subjective
+          outlook and surreal, mystic vision. “Tamerlane” and “Al Aaraaf”
+          exemplify Poe’s evolution from the portrayal of Byronic heroes to the
+          depiction of journeys within his own imagination and subconscious. The
+          former piece, reminiscent of Byron’s “Childe Harold’s Pilgrimage,”
+          recounts the life and adventures of a 14th-century Mongol conqueror;
+          the latter poem portrays a dreamworld where neither good nor evil
+          permanently reside and where absolute beauty can be directly
+          discerned. In other poems—“To Helen,” “Lenore,” and “The Raven” in
+          particular—Poe investigates the loss of ideal beauty and the
+          difficulty in regaining it. These pieces are usually narrated by a
+          young man who laments the untimely death of his beloved. “To Helen” is
+          a three stanza lyric that has been called one of the most beautiful
+          love poems in the English language. The subject of the work is a woman
+          who becomes, in the eyes of the narrator, a personification of the
+          classical beauty of ancient Greece and Rome. “Lenore” presents ways in
+          which the dead are best remembered, either by mourning or celebrating
+          life beyond earthly boundaries. In “The Raven,” Poe successfully
+          unites his philosophical and aesthetic ideals. In this psychological
+          piece, a young scholar is emotionally tormented by a raven’s ominous
+          repetition of “Nevermore” in answer to his question about the
+          probability of an afterlife with his deceased lover. Charles
+          Baudelaire noted in his introduction to the French edition of “The
+          Raven”: “It is indeed the poem of the sleeplessness of despair; it
+          lacks nothing: neither the fever of ideas, nor the violence of colors,
+          nor sickly reasoning, nor drivelling terror, nor even the bizarre
+          gaiety of suffering which makes it more terrible.” Poe also wrote
+          poems that were intended to be read aloud. Experimenting with
+          combinations of sound and rhythm, he employed such technical devices
+          as repetition, parallelism, internal rhyme, alliteration, and
+          assonance to produce works that are unique in American poetry for
+          their haunting, musical quality. In “The Bells,” for example, the
+          repetition of the word “bells” in various structures accentuates the
+          unique tonality of the different types of bells described in the poem.
+          While his works were not conspicuously acclaimed during his lifetime,
+          Poe did earn due respect as a gifted fiction writer, poet, and man of
+          letters, and occasionally he achieved a measure of popular success,
+          especially following the appearance of “The Raven.” After his death,
+          however, the history of his critical reception becomes one of
+          dramatically uneven judgments and interpretations. This state of
+          affairs was initiated by Poe’s one-time friend and literary executor
+          R.W. Griswold, who, in a libelous obituary notice in the New York
+          Tribune bearing the byline “Ludwig,” attributed the depravity and
+          psychological aberrations of many of the characters in Poe’s fiction
+          to Poe himself. In retrospect, Griswold’s vilifications seem
+          ultimately to have elicited as much sympathy as censure with respect
+          to Poe and his work, leading subsequent biographers of the late 19th
+          century to defend, sometimes too devotedly, Poe’s name. It was not
+          until the 1941 biography by A.H. Quinn that a balanced view was
+          provided of Poe, his work, and the relationship between the author’s
+          life and his imagination. Nevertheless, the identification of Poe with
+          the murderers and madmen of his works survived and flourished in the
+          20th century, most prominently in the form of psychoanalytical studies
+          such as those of Marie Bonaparte and Joseph Wood Krutch. Added to the
+          controversy over the sanity, or at best the maturity of Poe (Paul
+          Elmer More called him “the poet of unripe boys and unsound men”), was
+          the question of the value of Poe’s works as serious literature. At the
+          forefront of Poe’s detractors were such eminent figures as Henry
+          James, Aldous Huxley, and T.S. Eliot, who dismissed Poe’s works as
+          juvenile, vulgar, and artistically debased; in contrast, these same
+          works have been judged to be of the highest literary merit by such
+          writers as Bernard Shaw and William Carlos Williams. Complementing
+          Poe’s erratic reputation among English and American critics is the
+          more stable, and generally more elevated opinion of critics elsewhere
+          in the world, particularly in France. Following the extensive
+          translations and commentaries of Charles Baudelaire in the 1850s,
+          Poe’s works were received with a peculiar esteem by French writers,
+          most profoundly those associated with the late 19th-century movement
+          of Symbolism, who admired Poe’s transcendent aspirations as a poet;
+          the 20th-century movement of Surrealism, which valued Poe’s bizarre
+          and apparently unruled imagination; and such figures as Paul Valéry,
+          who found in Poe’s theories and thought an ideal of supreme
+          rationalism. In other countries, Poe’s works have enjoyed a similar
+          regard, and numerous studies have been written tracing the influence
+          of the American author on the international literary scene, especially
+          in Russia, Japan, Scandinavia, and Latin America. Today, Poe is
+          recognized as one of the foremost progenitors of modern literature,
+          both in its popular forms, such as horror and detective fiction, and
+          in its more complex and self-conscious forms, which represent the
+          essential artistic manner of the 20th century. In contrast to earlier
+          critics who viewed the man and his works as one, criticism of the past
+          25 years has developed a view of Poe as a detached artist who was more
+          concerned with displaying his virtuosity than with expressing his
+          soul, and who maintained an ironic rather than an autobiographical
+          relationship to his writings. While at one time critics such as Yvor
+          Winters wished to remove Poe from literary history, his works remain
+          integral to any conception of modernism in world literature. Herbert
+          Marshall McLuhan wrote in an essay entitled “Edgar Poe’s Tradition”:
+          “While the New England dons primly turned the pages of Plato and
+          Buddha beside a tea-cozy, and while Browning and Tennyson were
+          creating a parochial fog for the English mind to relax in, Poe never
+          lost contact with the terrible pathos of his time. Coevally with
+          Baudelaire, and long before Conrad and Eliot, he explored the heart of
+          darkness.”
+        </Typography>
+      </Stack>
+    </Box>
+  );
 }
 
 export default PaginaArtista;
