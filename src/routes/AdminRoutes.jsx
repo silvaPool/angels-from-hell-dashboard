fa62a54158bd5ec4aada0f9797c5ab62f24bba9d
silvaPool <silva.friends22@gmail.com>
2024-08-13 11:06:13 -0300

    AdminGuard

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,12 +1,15 @@
 import AdminHome from "../pages/admin/AdminHome";
+import AdminGuard from "../utils/AdminGuard";
 
 const AdminRoutes = [
-    {
-        path: "/admin",
-        element: (
-            <AdminHome />
-        ),
-    },
+  {
+    path: "/admin",
+    element: (
+      <AdminGuard>
+        <AdminHome />
+      </AdminGuard>
+    ),
+  },
 ];
 
 export default AdminRoutes;
---
+++ src/utils/AdminGuard.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate, useNavigate } from "react-router-dom";
+
+function AdminGuard({children}) {
+    
+    const {signed, loading, user} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    return !loading ?
+    (!signed ? (<Navigate to={"/login"} />) : (user.email === 'admin@admin.com'? children : navigate("/home/usuario"))) : undefined;
+}
+
+export default AdminGuard;
