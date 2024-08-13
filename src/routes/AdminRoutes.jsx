bf2a6cb0c62f5d80ee10c9d325891b316d5c346b
silvaPool <silva.friends22@gmail.com>
2024-08-13 10:20:50 -0300

    Rota de admin criada

---
+++ src/routes/AdminRoutes.jsx
@@ -0,0 +1,12 @@
+import AdminHome from "../pages/admin/AdminHome";
+
+const AdminRoutes = [
+    {
+        path: "/admin",
+        element: (
+            <AdminHome />
+        ),
+    },
+];
+
+export default AdminRoutes;
