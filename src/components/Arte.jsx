480063dd9ec9f91b84f0d81516f26c209d41c131
silvaPool <silva.friends22@gmail.com>
2024-08-22 11:15:43 -0300

    map de cada info no componente Arte

--- src/components/Arte.jsx
+++ src/components/Arte.jsx
@@ -1,18 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import Ghost from "../assets/images/ghost.png";
 import Abo from "../assets/images/abo.png";
+import cards from "../services/data";
+import { Fragment } from "react";
 
-function Arte() {
+{
+  /* <div class="book">
+<p>Explore</p>
+<div class="cover">
+  <img src={Ghost} alt="icon ghost" className="icon-1" />
+  <p>Edgar A. Poe</p>
+  <img src={Abo} alt="icon ghost" className="icon-2" />
+</div>
+</div> */
+}
+
+function Arte({ item }) {
   return (
     <Box sx={{ padding: "30px" }}>
-      <div class="book">
-        <p>Explore</p>
-        <div class="cover">
-          <img src={Ghost} alt="icon ghost" className="icon-1" />
-          <p>Edgar A. Poe</p>
-          <img src={Abo} alt="icon ghost" className="icon-2" />
-        </div>
-      </div>
+      <Grid container spacing={4}>
+        {cards && cards.length > 0 ? (
+          cards.map((item, index) => (
+            <Fragment key={index}>
+              <div class="book">
+                <p>Explore</p>
+                <div class="cover">
+                  <img src={Ghost} alt="icon ghost" className="icon-1" />
+                  <p>{item.nomeArtista}</p>
+                  <img src={Abo} alt="icon ghost" className="icon-2" />
+                </div>
+              </div>
+            </Fragment>
+          ))
+        ) : (
+          <Grid item sx={{ color: "black", fontSize: "2rem" }}>
+            <p>Nada</p>
+          </Grid>
+        )}
+      </Grid>
     </Box>
   );
 }
