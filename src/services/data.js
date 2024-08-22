f75c5cd65ddb6c56e8964fb9b9f42162ca221e29
silvaPool <silva.friends22@gmail.com>
2024-08-22 11:05:03 -0300

    arquivo data criado para o componente de cards

---
+++ src/services/data.js
@@ -0,0 +1,20 @@
+const cards = [
+    {
+        id: 1,
+        nomeArtista: "Edgar A. Poe",
+    },
+    {
+        id: 2,
+        nomeArtista: "Exemplo 2",
+    },
+    {
+        id: 3,
+        nomeArtista: "Exemplo 3",
+    },
+    {
+        id: 4,
+        nomeArtista: "Exemplo 4",
+    }
+]
+
+export default cards;
