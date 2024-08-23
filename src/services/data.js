df4f9c2e6d1c58618473347ac82fd296719ffa00
silvaPool <silva.friends22@gmail.com>
2024-08-23 09:06:47 -0300

    mais info no arquivo data

--- src/services/data.js
+++ src/services/data.js
@@ -5,15 +5,39 @@ const cards = [
     },
     {
         id: 2,
-        nomeArtista: "Exemplo 2",
+        nomeArtista: "Stephen King",
     },
     {
         id: 3,
-        nomeArtista: "Exemplo 3",
+        nomeArtista: "Anne Rice",
     },
     {
         id: 4,
-        nomeArtista: "Exemplo 4",
+        nomeArtista: "H.P. Lovecraft",
+    },
+    {
+        id: 5,
+        nomeArtista: "August W. Derleth",
+    },
+    {
+        id: 6,
+        nomeArtista: "Clive Barker",
+    },
+    {
+        id: 7,
+        nomeArtista: "Robert A. Bloch",
+    },
+    {
+        id: 8,
+        nomeArtista: "Robert W. Chambers",
+    },
+    {
+        id: 9,
+        nomeArtista: "Charles Beaumont",
+    },
+    {
+        id: 10,
+        nomeArtista: "Clark A. Smith",
     }
 ]
 
