8f4438f4e8929cecb2efe087a7f48d9d04718945
silvaPool <silva.friends22@gmail.com>
2024-08-23 10:39:36 -0300

    info de livros no arquivo data

--- src/services/data.js
+++ src/services/data.js
@@ -1,43 +1,71 @@
 const cards = [
     {
+        type: "autor",
         id: 1,
         nomeArtista: "Edgar A. Poe",
     },
     {
+        type: "autor",
         id: 2,
         nomeArtista: "Stephen King",
     },
     {
+        type: "autor",
         id: 3,
         nomeArtista: "Anne Rice",
     },
     {
+        type: "autor",
         id: 4,
         nomeArtista: "H.P. Lovecraft",
     },
     {
+        type: "autor",
         id: 5,
         nomeArtista: "August W. Derleth",
     },
     {
+        type: "autor",
         id: 6,
         nomeArtista: "Clive Barker",
     },
     {
+        type: "autor",
         id: 7,
         nomeArtista: "Robert A. Bloch",
     },
     {
+        type: "autor",
         id: 8,
         nomeArtista: "Robert W. Chambers",
     },
     {
+        type: "autor",
         id: 9,
         nomeArtista: "Charles Beaumont",
     },
     {
+        type: "autor",
         id: 10,
         nomeArtista: "Clark A. Smith",
+    },
+    {
+        type: "livro",
+        id: 11,
+        nomeArtista: "Edgar A. Poe",
+        nomeLivro: "O Corvo",
+    },
+    {
+        type: "livro",
+        id: 12,
+        nomeArtista: "Exemplo 2",
+        nomeLivro: "Bla",
+    },
+    {
+        type: "livro",
+        id: 13,
+        nomeArtista: "Exemplo 3",
+        nomeLivro: "blabla",
     }
 ]
 
