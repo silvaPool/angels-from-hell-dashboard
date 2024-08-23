cef62090ce8a5683519983c4e270a1155de59f4c
silvaPool <silva.friends22@gmail.com>
2024-08-23 10:51:17 -0300

    Mapeando pelo type no componente livros

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import './App.css'
-import Arte from './components/Arte'
-import HomeUsuario from './components/HomeUsuario'
-import PaginaArtista from './components/PaginaArtista'
-import { Auth } from './context/AuthContext'
-import Cadastro from './pages/Cadastro'
-import Login from './pages/Login'
-import Routes from './routes/AppRoutes'
+import "./App.css";
+import Arte from "./components/Arte";
+import HomeUsuario from "./components/HomeUsuario";
+import Livros from "./components/Livros";
+import PaginaArtista from "./components/PaginaArtista";
+import { Auth } from "./context/AuthContext";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
+import Routes from "./routes/AppRoutes";
 
 function App() {
- 
-
   return (
     <>
-
       {/* <Auth>
         <Routes />
       </Auth> */}
@@ -21,10 +19,11 @@ function App() {
 
       {/* <Arte /> */}
 
-      <PaginaArtista />
-      
+      {/* <PaginaArtista /> */}
+
+      <Livros />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Livros.jsx
+++ src/components/Livros.jsx
@@ -1,9 +1,31 @@
+import cards from "../services/data";
+import { Box, Grid, Typography } from "@mui/material";
+import cardsLivros from "../services/data";
+import { Fragment } from "react";
+
 function Livros() {
+
+    const livros = cardsLivros.filter(item => item.type === "livro")
     
     return (
-        <div>
-            dkdklsdsl
-        </div>
+        <Box sx={{ padding: "30px" }}>
+        <Grid container spacing={1} gap={"15px"}>
+          {livros && livros.length > 0 ? (
+            livros.map((item, index) => (
+              <Fragment key={index}>
+                <div>
+                    {item.nomeLivro}
+                    {item.nomeArtista}
+                </div>
+              </Fragment>
+            ))
+          ) : (
+            <Grid item sx={{ color: "black", fontSize: "2rem" }}>
+              <p>Nada</p>
+            </Grid>
+          )}
+        </Grid>
+      </Box>
     )
 }
 
